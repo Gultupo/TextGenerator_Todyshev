07ce68ac658c6541993582095e9dcbae5edd1aa2
Gultupo <80353369+Gultupo@users.noreply.github.com>
2021-04-25 22:53:18 +0300

    Add files via upload

---
+++ generation.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+fr_nw = requests.post("https://pelevin.gpt.dobro.ai/generate/",
+                                  data=json.dumps({"prompt": 'Он шел по лесу.', "length": 30})).json()
+fr_nw = fr_nw['replies'][0]
+print(fr_nw)
---
+++ sandbox.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import os
+
+token = 'AQAAAAA1ZV4rAAT7ozFhxJ47VUYgkXM9IImE1zk'
+url = 'https://dialogs.yandex.net/api/v1/status'
+
+headers = {'Host': 'https://dialogs.yandex.net/api/v1/status', f'Authorization': f'OAuth {token}'}
+
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
